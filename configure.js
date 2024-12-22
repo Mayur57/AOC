aa3f62976daef95ce8067d91b149931ebd7e54ac
Mayur Bhoi <mayur072000@gmail.com>
2024-12-22 16:26:20 +0530

    add: network utils

--- configure.js
+++ configure.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 if (args.length !== 1) {
-    console.error("Usage: node configure.js <string>");
-    process.exit(1);
+  console.error('Usage: node configure.js <string>')
+  process.exit(1)
 }
 
-const sessionString = args[0];
+const sessionString = args[0]
 
 const config = {
-    session: sessionString
-};
+  session: sessionString,
+}
 
-fs.writeFile("config.json", JSON.stringify(config, null, 4), (err) => {
-    if (err) {
-        console.error("[ERR] Error writing to config.json:", err);
-        process.exit(1);
-    }
-    console.log("[INF] config.json has been created successfully.");
-});
+fs.writeFile('config.json', JSON.stringify(config, null, 4), err => {
+  if (err) {
+    console.error('[ERR] Error writing to config.json:', err)
+    process.exit(1)
+  }
+  console.log('[INF] config.json has been created successfully.')
+})
